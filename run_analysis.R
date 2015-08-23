0a4eab26a27cb73e58c01f21fac4568172aa34db
Edna Tovar <ednatovar@Ednas-MacBook-Pro.local>
2015-08-23 15:50:56 -0500

    added merging

--- run_analysis.R
+++ run_analysis.R
@@ -1,21 +1,29 @@
 
+library(dplyr)
+library(data.table)
+
+#Read Training Data
 X_train_data <- read.table("train/X_train.txt", header = FALSE) 
 y_train_data <- read.table("train/y_train.txt", header = FALSE) 
 subject_train_data <- read.table("train/subject_train.txt", header = FALSE) 
 
+#Read Test Data
 X_test_data <- read.table("test/X_test.txt", header = FALSE) 
 y_test_data <- read.table("test/y_test.txt", header = FALSE) 
 subject_test_data <- read.table("test/subject_test.txt", header = FALSE) 
 
+#Merge Training and Test Data
 subjectdata <- rbind(subject_train_data, subject_test_data)
 activitydata <- rbind(y_train_data, y_test_data)
 featuresdata <- rbind(X_train_data, X_test_data)
 
-View(featuresdata)
-
-
+#Read Activity and Features Label Data
 activity_labels <- read.table("activity_labels.txt", header = TRUE) 
-
-
 features <- read.table("features.txt")
-View(features)
+
+#Use features column 2 transposed as features data column headers
+colnames(featuresdata) <-  t(features[2])
+
+View(activitydata)
+View(subjectdata)
+View(featuresdata)
